c3275f5878af04611d24090da3b6a0d727105090
MASAKI-knf <82574868+MASAKI-knf@users.noreply.github.com>
2021-06-02 19:07:20 +0900

    1-1 commit

---
+++ JavaStudy/1-1/Task1_1.java
@@ -0,0 +1,25 @@
+public class Task1_1 {
+
+    public static void main(String[] args){
+
+        int hogeInt = 100;
+        System.out.println("" + hogeInt);
+
+        String hogeString = "合格";
+
+        System.out.print(hogeString);
+
+
+        //double型の hogeDouble という名前の変数を宣言し、値「0.2」で初期化する処理をしている。
+        double hogeDouble = 0.2;
+        System.out.println("" + hogeDouble);
+
+        //hogeStringを表示すると言う処理をしている。
+
+
+    }
+}
+
+
+
+
