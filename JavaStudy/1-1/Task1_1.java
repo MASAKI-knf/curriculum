b5c941882387e566d7a300c624c83b336e855e5c
MASAKI-knf <82574868+MASAKI-knf@users.noreply.github.com>
2021-06-15 18:39:49 +0900

    Java第１章データ型課題再提出

---
+++ JavaStudy/1-1/Task1_1.java
@@ -0,0 +1,26 @@
+public class Task1_1 {
+
+    public static void main(String[] args) {
+
+        int hogeInt;
+        hogeInt = 50;
+        hogeInt = 100;
+        System.out.println(hogeInt);
+    
+
+        String hogeString = "テスト";       
+        hogeString = "合格";
+        System.out.print(hogeString);
+
+
+
+    //③double型を宣言して、「hogeDouble」という変数名をつけて0.2で初期化する。
+        double hogeDouble;
+        hogeDouble = 0.2;
+        System.out.println(hogeDouble);
+
+    //⑦hogeStringという変数名の箱の中身「値」を表示する処理を行っている。       
+
+
+    }
+}
