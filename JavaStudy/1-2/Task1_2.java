838926612188a37b300e3b57d4d955bc1c9821eb
MASAKI-knf <82574868+MASAKI-knf@users.noreply.github.com>
2021-06-17 18:57:42 +0900

    Java第１章配列課題

---
+++ JavaStudy/1-2/Task1_2.java
@@ -0,0 +1,25 @@
+public class Task1_2 {
+
+    public static void main(String[] args) {
+
+        String[] country = {"JAPAN", "AMERICA", "KOREA"};
+
+        System.out.println("配列countryの要素数…"+ country.length);
+
+
+        String[] strArray = {"りんご","もも","ぶどう"};
+
+        System.out.println("strArray[1]…"+strArray[1]);
+
+
+//⑤10、20、30、40、50を要素値とする、５個の要素数をintArrayという名前のint型配列で宣言している。
+        int[] intArray = {10,20,30,40,50};
+
+//⑥1番目と4番目の要素値を合計して出力している。
+        System.out.println(intArray[1]+intArray[4]);
+
+
+
+    }
+}
+
