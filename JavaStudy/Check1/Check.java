cadb154608be3451988723d2e6eb3fab3fb838fc
MASAKI-knf <82574868+MASAKI-knf@users.noreply.github.com>
2021-07-15 19:35:21 +0900

    test

--- JavaStudy/Check1/Check.java
+++ JavaStudy/Check1/Check.java
@@ -21,15 +21,15 @@ public class Check{
         }
         System.out.println();
         for (int i = 0; i < data.length -1; i++) {
-            for (int j = i+1; j > i; j--) {
+            for (int j = data.length -1; j > i; j--) {
                 /*
                 * 問3
                 * 以下、配列の添字を入れてソートを完成させなさい
                 */
-                if(data[i] > data[j]){
-                  int box = data[i];
-                  data[i] = data[j];
-                  data[j] = box;
+                if(data[j-1] > data[j]){
+                  int box = data[j];
+                  data[j] = data[j-1];
+                  data[j-1] = box;
                 }
             }
         }
