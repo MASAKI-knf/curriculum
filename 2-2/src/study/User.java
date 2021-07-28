b398ff89dd18709c26ead640ec1f09c406ee7b10
MASAKI-knf <82574868+MASAKI-knf@users.noreply.github.com>
2021-07-28 23:53:22 +0900

    Java2-2

---
+++ 2-2/src/study/User.java
@@ -0,0 +1,29 @@
+package study;
+
+
+/**
+ * User.java
+ */
+class User {
+
+    // フィールド変数
+    private String userName;
+    private int id;
+    private String password;
+
+    // コンストラクタ
+    public User(String userName, int id, String password) {
+        this.userName = userName;
+        this.id = id;
+        this.password = password;
+    }
+
+    // ① 以下のルールに従いアカウント情報（名前、ID、パスワード）を出力するメソッドを作成してください。
+    // アクセス修飾子:「protected」
+    // メソッド名:「printAccountInfo」
+    protected void printAccountInfo() {
+    	System.out.println("ユーザ名、" + userName +" "+"IDは、" + id +" "+ "パスワードは、" + password );
+    }
+
+
+}
